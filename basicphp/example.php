989d6f227ee3e62bbc4d1e50bb93b26b1bf03cb6
Gekif <dzulfikar.maulana@gmail.com>
2020-03-24 18:43:03 +0700

    2. Building Blocks of PHP: 6. Document overview

---
+++ basicphp/example.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Bio</title>
+</head>
+<body>
+
+<h1>My Bio</h1>
+
+<?php
+echo "My name is fikar";
+?>
+<br>
+<?php echo "who is fikar"; ?> <br>
+<?php echo "how old is he?"; ?> <br>
+<?php echo 30 + 2; ?>
+
+</body>
+</html>
