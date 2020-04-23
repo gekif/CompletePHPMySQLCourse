131c43e67b2abfca7476831acb45d39e2c44b2e7
Gekif <dzulfikar.maulana@gmail.com>
2020-04-23 10:13:13 +0700

    2. Building Blocks of PHP: 8. Constants

---
+++ basicphp/contants.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Constant</title>
+</head>
+<body>
+
+<?php
+$name = 'Fikar';
+
+define('Value_of_Pi', 3.14);
+define('Value_of_Gravity', 9.8);
+
+echo 'PI VALUE: ' .  Value_of_Pi;
+echo '<br>';
+echo 'GRAVITY VALUE: ' . Value_of_Gravity;
+
+?>
+
+</body>
+</html>
