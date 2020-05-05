b6a27959fc6458d2a6ca3b459ed8ad0f15bb8b63
Gekif <dzulfikar.maulana@gmail.com>
2020-05-05 21:19:53 +0700

    4. Building Blocks of PHP Continue: 10. Re-usability

---
+++ basicphp/function.php
@@ -0,0 +1,12 @@
+<?php
+function welcome()
+{
+    echo "Welcome to the courses";
+}
+
+function addition_with_parameter($a, $b)
+{
+    return $a + $b;
+}
+
+?>
---
+++ basicphp/reusability.php
@@ -0,0 +1,19 @@
+<?php include('function.php') ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Reusability</title>
+</head>
+<body>
+
+<?php
+echo addition_with_parameter(20, 30);
+?>
+
+</body>
+</html>
